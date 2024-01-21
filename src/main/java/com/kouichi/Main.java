eceb46c527f064ce8416998e0c0a88c110edf922
”溝口光一” <guangyigoukou7@gmail.com>
2024-01-21 14:53:34 +0900

    for文の条件をiからjまでに変更

--- src/main/java/com/kouichi/Main.java
+++ src/main/java/com/kouichi/Main.java
@@ -5,11 +5,11 @@ package com.kouichi;
 public class Main {
     public static void main(String[] args) {
 
-        int j=10;
-        for (int i = 0; i < 20; i++) {
-            if (i <=10){
+        int j=20;
+        for (int i = 0; i < j; i++) {
+            if (i <= 10){
                 System.out.println("hello world");
-            } else if (i <=20){
+            } else if (10 < i){
                 System.out.println("hello");
 
             }
